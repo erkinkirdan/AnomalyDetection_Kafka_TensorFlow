0b266466c2ec75dad85469b8b0b8b8a2435fae1b
erkinkirdan <erkinkirdan@users.noreply.github.com>
2023-08-08 22:33:18 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -40,6 +40,45 @@ def eval_performance(pred_file):
     stream_latency_sorted = np.sort(stream_latency)
     prediction_latency_sorted = np.sort(prediction_latency)
     total_latency_sorted = np.sort(total_latency)
+    
+    # Cumulative Distribution Function (CDF)
+    cdf_stream_latency = np.arange(len(stream_latency)) / float(len(stream_latency))
+    cdf_prediction_latency = np.arange(len(prediction_latency)) / float(len(prediction_latency))
+    cdf_total_latency = np.arange(len(total_latency)) / float(len(total_latency))
+
+    # Cumulative Distribution Function (CDF)
+    plt.figure(figsize=(10, 7))
+    plt.plot(stream_latency_sorted, cdf_stream_latency, label='Stream Latency', linewidth=2.0)
+    plt.plot(prediction_latency_sorted, cdf_prediction_latency, label='Prediction Latency', linewidth=2.0)
+    plt.plot(total_latency_sorted, cdf_total_latency, label='Total Latency', linewidth=2.0)
+    plt.xscale('log')  # Set the x-axis to a logarithmic scale
+
+    # Add a vertical line for the 50th percentile
+    plt.axhline(y=0.5, color='r', linestyle='--', linewidth=2.0)
+    plt.text(0.9, 0.5, '50th Percentile', color='r', va='center', ha='left', backgroundcolor='w', fontweight='bold')
+
+    # Get the x values where the line crosses the cdf plots for 50th percentile
+    x_stream_latency_50 = np.interp(0.5, cdf_stream_latency, stream_latency_sorted)
+    x_prediction_latency_50 = np.interp(0.5, cdf_prediction_latency, prediction_latency_sorted)
+    x_total_latency_50 = np.interp(0.5, cdf_total_latency, total_latency_sorted)
+
+    # Annotate the intersection points for 50th percentile
+    plt.annotate(f'{x_stream_latency_50:.2f}', xy=(x_stream_latency_50, 0.5), xycoords='data',
+                 xytext=(-30, -30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
+    plt.annotate(f'{x_prediction_latency_50:.2f}', xy=(x_prediction_latency_50, 0.5), xycoords='data',
+                 xytext=(30, 30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
+    plt.annotate(f'{x_total_latency_50:.2f}', xy=(x_total_latency_50, 0.5), xycoords='data',
+                 xytext=(30, 30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
+
+    plt.title('CDF of Latencies', fontweight='bold')
+    plt.xlabel('Latency (ms)', fontweight='bold')
+    plt.ylabel('CDF', fontweight='bold')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig('Latency_CDF.pdf')
 
     ccdf_stream_latency = 1 - np.arange(len(stream_latency)) / len(stream_latency)
     ccdf_prediction_latency = 1 - np.arange(len(prediction_latency)) / len(prediction_latency)
@@ -51,27 +90,39 @@ def eval_performance(pred_file):
     plt.loglog(prediction_latency_sorted, ccdf_prediction_latency, label='Prediction Latency', linewidth=2.0)
     plt.loglog(total_latency_sorted, ccdf_total_latency, label='Total Latency', linewidth=2.0)
 
-    # Add vertical lines at specified percentiles
-    percentiles = [(10 ** -2, '99th Percentile'), (10 ** -4, '99.99th Percentile'), (10 ** -0.3, '50th Percentile')]
-    for y_line, percentile_label in percentiles:
-        plt.axhline(y=y_line, color='r', linestyle='--', linewidth=2.0)
-        plt.text(0.9, y_line, percentile_label, color='r', va='center', ha='right', backgroundcolor='w', fontweight='bold')
+    # 99th Percentile
+    y_line_99 = 10 ** -2
+    plt.axhline(y=y_line_99, color='r', linestyle='--', linewidth=2.0)
+    plt.text(0.9, y_line_99, '99th Percentile', color='r', va='center', ha='left', backgroundcolor='w', fontweight='bold')
+    x_stream_latency_99 = np.interp(y_line_99, ccdf_stream_latency[::-1], stream_latency_sorted[::-1])
+    x_prediction_latency_99 = np.interp(y_line_99, ccdf_prediction_latency[::-1], prediction_latency_sorted[::-1])
+    x_total_latency_99 = np.interp(y_line_99, ccdf_total_latency[::-1], total_latency_sorted[::-1])
+    plt.annotate(f'{x_stream_latency_99:.2f}', xy=(x_stream_latency_99, y_line_99), xycoords='data',
+                 xytext=(-30, -30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
+    plt.annotate(f'{x_prediction_latency_99:.2f}', xy=(x_prediction_latency_99, y_line_99), xycoords='data',
+                 xytext=(30, 30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
+    plt.annotate(f'{x_total_latency_99:.2f}', xy=(x_total_latency_99, y_line_99), xycoords='data',
+                 xytext=(30, 30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
 
-        # Get the x values where the line crosses the ccdf plots
-        x_stream_latency = np.interp(y_line, ccdf_stream_latency[::-1], stream_latency_sorted[::-1])
-        x_prediction_latency = np.interp(y_line, ccdf_prediction_latency[::-1], prediction_latency_sorted[::-1])
-        x_total_latency = np.interp(y_line, ccdf_total_latency[::-1], total_latency_sorted[::-1])
-
-        # Annotate the intersection points
-        plt.annotate(f'{x_stream_latency:.2f}', xy=(x_stream_latency, y_line), xycoords='data',
-                     xytext=(-30, -30), textcoords='offset points', fontweight='bold',
-                     arrowprops=dict(arrowstyle="->"))
-        plt.annotate(f'{x_prediction_latency:.2f}', xy=(x_prediction_latency, y_line), xycoords='data',
-                     xytext=(30, 30), textcoords='offset points', fontweight='bold',
-                     arrowprops=dict(arrowstyle="->"))
-        plt.annotate(f'{x_total_latency:.2f}', xy=(x_total_latency, y_line), xycoords='data',
-                     xytext=(30, 30), textcoords='offset points', fontweight='bold',
-                     arrowprops=dict(arrowstyle="->"))
+    # 99.99th Percentile
+    y_line_9999 = 10 ** -4
+    plt.axhline(y=y_line_9999, color='r', linestyle='--', linewidth=2.0)
+    plt.text(0.9, y_line_9999, '99.99th Percentile', color='r', va='center', ha='right', backgroundcolor='w', fontweight='bold')
+    x_stream_latency_9999 = np.interp(y_line_9999, ccdf_stream_latency[::-1], stream_latency_sorted[::-1])
+    x_prediction_latency_9999 = np.interp(y_line_9999, ccdf_prediction_latency[::-1], prediction_latency_sorted[::-1])
+    x_total_latency_9999 = np.interp(y_line_9999, ccdf_total_latency[::-1], total_latency_sorted[::-1])
+    plt.annotate(f'{x_stream_latency_9999:.2f}', xy=(x_stream_latency_9999, y_line_9999), xycoords='data',
+                 xytext=(-30, -30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
+    plt.annotate(f'{x_prediction_latency_9999:.2f}', xy=(x_prediction_latency_9999, y_line_9999), xycoords='data',
+                 xytext=(30, 30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
+    plt.annotate(f'{x_total_latency_9999:.2f}', xy=(x_total_latency_9999, y_line_9999), xycoords='data',
+                 xytext=(30, 30), textcoords='offset points', fontweight='bold',
+                 arrowprops=dict(arrowstyle="->"))
 
     plt.title('CCDF of Latencies', fontweight='bold')
     plt.xlabel('Latency (ms)', fontweight='bold')
